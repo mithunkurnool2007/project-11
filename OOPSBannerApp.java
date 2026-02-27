e088eaa5ba591274025f03242c0d7ffc3dae38c2
Mithun <mithunkurnool@gmail.com>
2026-02-28 03:07:42 +0530

    UC4 completed

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,17 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *     *");
-        System.out.println("*     *  *     *  *     *  *     *");
-        System.out.println("*     *  *     *  *****   ***** ");
-        System.out.println("*     *  *     *  *       *     ");
-        System.out.println("*     *  *     *  *       *     ");
-        System.out.println(" *****    *****   *       ***** ");
+        String[] banner = {
+                "*****   *****   *****   *****",
+                "*   *   *   *   *   *   *   *",
+                "*   *   *   *   *   *   *   *",
+                "*   *   *   *   *   *   *   *",
+                "*****   *****   *****   *****"
+        };
 
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
