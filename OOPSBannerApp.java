81982e231dfe2887d32ac85d1342976fd2ccaa63
Mithun <mithunkurnool@gmail.com>
2026-03-02 13:12:08 +0530

    UC5 completed

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,14 @@
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
 
         String[] banner = {
-                "*****   *****   *****   *****",
-                "*   *   *   *   *   *   *   *",
-                "*   *   *   *   *   *   *   *",
-                "*   *   *   *   *   *   *   *",
-                "*****   *****   *****   *****"
+            String.join("   ", " **** ", " **** ", " **** ", " **** "),
+            String.join("   ", "*    *", "*    *", "*    *", "*"),
+            String.join("   ", "*    *", "*    *", " **** ", " **** "),
+            String.join("   ", "*    *", "*    *", "*", "     *"),
+            String.join("   ", "*    *", "*    *", "*", "     *"),
+            String.join("   ", "*    *", "*    *", "*", "*    *"),
+            String.join("   ", " **** ", " **** ", "*", " **** ")
         };
 
         for (String line : banner) {
